6e4952a86fae3af6adfeabab38231c30c00eaf5f
tknemuru <kojikoji.max.5252@gmail.com>
2021-05-01 17:11:36 +0900

    feat: mod template.

--- Arman/Questions/AbcTemplate.cs
+++ Arman/Questions/AbcTemplate.cs
@@ -28,19 +28,16 @@ namespace Arman.Questions
             // 整数配列の入力
             var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
 
-
-
-
-            string result = "";
+            string result = Resolve(inputLongArray);
 
             Console.WriteLine(result);
 
             Console.Out.Flush();
         }
 
-        public static long Resolve(long n)
+        public static String Resolve(long[] nums)
         {
-            return n * n;
+            return (nums[0] * nums[1]).ToString();
         }
     }
 }
