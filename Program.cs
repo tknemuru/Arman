ff176454f77036f0cf85df2906491e8f844794cf
tknemuru <kojikoji.max.5252@gmail.com>
2021-05-01 09:24:37 +0900

    feat: first commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
