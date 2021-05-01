fa2c7c2a6d5f32a135920cac36a3dec18cc76a8d
tknemuru <kojikoji.max.5252@gmail.com>
2021-05-01 17:57:23 +0900

    feat: add abc081B.

---
+++ Arman.Tests/Questions/Abc081BTest.cs
@@ -0,0 +1,20 @@
+using Arman.Questions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arman.Tests
+{
+    [TestClass]
+    public class Abc081BTest : BaseUnitTest
+    {
+        [TestMethod]
+        public void 基本ケース()
+        {
+            var actual = Abc081B.Resolve("3", new string[] { "8", "12", "40" });
+            Assert.AreEqual(2, actual);
+            actual = Abc081B.Resolve("4", new string[] { "5", "6", "8", "10" });
+            Assert.AreEqual(0, actual);
+            actual = Abc081B.Resolve("6", new string[] { "382253568", "723152896", "37802240", "379425024", "404894720", "471526144" });
+            Assert.AreEqual(8, actual);
+        }
+    }
+}
---
+++ Arman/Questions/Abc081B.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arman.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc081/tasks/abc081_a
+    /// </summary>
+    public class Abc081B
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            // 文字列の入力
+            string s1 = Console.ReadLine();
+            var s2 = Console.ReadLine().Split(' ');
+
+            //// 整数の入力
+            //long n = long.Parse(Console.ReadLine());
+
+            //// 文字列配列の入力
+            //string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            //// 整数配列の入力
+            //var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            var result = Resolve(s1, s2);
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+
+        public static long Resolve(string N, string[] strs)
+        {
+            var count = 0L;
+            var nums = strs.Select(s => long.Parse(s));
+            while (true)
+            {
+                var has = nums.All(n => n % 2 == 0);
+                if (!has)
+                {
+                    break;
+                }
+                count++;
+                nums = nums.Select(n => n / 2);
+            }
+            return count;
+        }
+    }
+}
