1458bb4697358cb03d7c5087e329d864d7b24bf2
tknemuru <kojikoji.max.5252@gmail.com>
2021-05-02 14:29:18 +0900

    feat: add tle.

---
+++ Arman.Tests/Questions/Abc085CTest.cs
@@ -0,0 +1,22 @@
+﻿using Arman.Questions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arman.Tests
+{
+    [TestClass]
+    public class Abc085CTest : BaseUnitTest
+    {
+        [TestMethod]
+        public void 基本ケース()
+        {
+            var actual = Abc085C.Resolve("9 45000");
+            Assert.AreEqual("4 0 5", actual);
+            actual = Abc085C.Resolve("20 196000");
+            Assert.AreEqual("-1 -1 -1", actual);
+            actual = Abc085C.Resolve("1000 1234000");
+            Assert.AreEqual("26 0 974", actual);
+            actual = Abc085C.Resolve("2000 20000000");
+            Assert.AreEqual("2000 0 0", actual);
+        }
+    }
+}
---
+++ Arman/Questions/Abc085C.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arman.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc085/tasks/abc085_c
+    /// </summary>
+    public class Abc085C
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            // 文字列の入力
+            string s = Console.ReadLine();
+
+            //// 整数の入力
+            //long n = long.Parse(Console.ReadLine());
+
+            //// 文字列配列の入力
+            //string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            //// 整数配列の入力
+            //var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            var result = Resolve(s);
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+
+        public static string Resolve(string inputStr)
+        {
+            var input = inputStr.Split(' ').Select(i => long.Parse(i)).ToArray();
+            var N = input[0];
+            var Y = input[1];
+            var maxTen = Math.Min(Y / 10000L, N);
+            var maxFive = Math.Min(Y / 5000L, N);
+            var maxOne = Math.Min(Y / 1000L, N);
+            var answer = "-1 -1 -1";
+            for (var t = maxTen; 0L <= t; t--)
+            {
+                for (var f = maxFive; 0L <= f; f--)
+                {
+                    for (var o = maxOne; 0L <= o; o--)
+                    {
+                        var sumNum = t + f + o;
+                        var sumMoney = t * 10000L + f * 5000L + o * 1000L;
+                        if (sumNum == N && sumMoney == Y)
+                        {
+                            answer = $"{t} {f} {o}";
+                            return answer;
+                        }
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
