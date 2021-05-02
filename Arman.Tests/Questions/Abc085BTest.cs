a0d9fb8d9b625f34296afada37e08ab9ab6ad4ad
tknemuru <kojikoji.max.5252@gmail.com>
2021-05-02 13:55:31 +0900

    feat: add.

---
+++ Arman.Tests/Questions/Abc085BTest.cs
@@ -0,0 +1,20 @@
+﻿using Arman.Questions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arman.Tests
+{
+    [TestClass]
+    public class Abc085BTest : BaseUnitTest
+    {
+        [TestMethod]
+        public void 基本ケース()
+        {
+            var actual = Abc085B.Resolve(4, new long[] { 10, 8, 8, 6 });
+            Assert.AreEqual(3, actual);
+            actual = Abc085B.Resolve(3, new long[] { 15, 15, 15 });
+            Assert.AreEqual(1, actual);
+            actual = Abc085B.Resolve(7, new long[] { 50, 30, 50, 100, 50, 80, 30 });
+            Assert.AreEqual(4, actual);
+        }
+    }
+}
---
+++ Arman/Questions/Abc085B.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arman.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc085/tasks/abc085_b
+    /// </summary>
+    public class Abc085B
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            //// 文字列の入力
+            //string s = Console.ReadLine();
+
+            //// 整数の入力
+            //long n = long.Parse(Console.ReadLine());
+
+            //// 文字列配列の入力
+            //string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            //// 整数配列の入力
+            //var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            long N = long.Parse(Console.ReadLine());
+            var nums = new long[N];
+            for (var i = 0; i < N; i++)
+            {
+                nums[i] = long.Parse(Console.ReadLine());
+            }
+            var result = Resolve(N, nums);
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+
+        public static long Resolve(long N, long[] nums)
+        {
+            return nums.Distinct().Count();
+        }
+    }
+}
