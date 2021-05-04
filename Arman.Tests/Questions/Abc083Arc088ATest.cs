ce3015ce8f38f43520032360e39b1da44ef5a055
tknemuru <kojikoji.max.5252@gmail.com>
2021-05-04 23:40:19 +0900

    feat: add.

---
+++ Arman.Tests/Questions/Abc083Arc088ATest.cs
@@ -0,0 +1,61 @@
+﻿using Arman.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Arman.Tests
+{
+    [TestClass]
+    public class Abc083Arc088ATest : BaseUnitTest
+    {
+        [TestMethod]
+        public void 基本ケース()
+        {
+            //Test();
+            TestFromFile();
+        }
+
+        /// <summary>
+        /// 直接パラメータを指定してテストを実行します。
+        /// </summary>
+        private void Test()
+        {
+            var actual = Abc083Arc088A.Resolve(new string[]
+            {
+                "aX",
+                "aX",
+                "aX",
+            });
+            Assert.AreEqual(9, actual);
+
+            actual = Abc083Arc088A.Resolve(new string[]
+            {
+                "aX",
+                "aX",
+                "aX",
+            });
+            Assert.AreEqual(9, actual);
+        }
+
+        /// <summary>
+        /// ファイルからパラメータを読み込んでテストを実行します。
+        /// </summary>
+        private void TestFromFile()
+        {
+            var input = this.ReadResource(1, 1, ResourceType.In).ToArray();
+            var actual = Abc083Arc088A.Resolve(input);
+            Assert.AreEqual(3, actual);
+
+            input = this.ReadResource(1, 2, ResourceType.In).ToArray();
+            actual = Abc083Arc088A.Resolve(input);
+            Assert.AreEqual(3, actual);
+
+            input = this.ReadResource(1, 3, ResourceType.In).ToArray();
+            actual = Abc083Arc088A.Resolve(input);
+            Assert.AreEqual(31, actual);
+        }
+    }
+}
---
+++ Arman/Questions/Abc083Arc088A.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arman.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc083/tasks/arc088_a
+    /// </summary>
+    public class Abc083Arc088A
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            //// 文字列の入力
+            //string s = Console.ReadLine();
+
+            //// 整数の入力
+            //long n = long.Parse(Console.ReadLine());
+
+            //// 文字列配列の入力
+            //string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            //// 整数配列の入力
+            //var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            var line = string.Empty;
+            var inputs = new List<string>();
+            while (true)
+            {
+                line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+                inputs.Add(line);
+            }
+            var result = Resolve(inputs.ToArray());
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+
+        public static long Resolve(string[] inputs)
+        {
+            var nums = inputs[0].Split(' ').Select(i => long.Parse(i)).ToArray();
+            var curr = nums[0];
+            var max = nums[1];
+            var count = 0L;
+            while (curr <= max)
+            {
+                count++;
+                curr *= 2;
+            }
+            return count;
+        }
+    }
+}
